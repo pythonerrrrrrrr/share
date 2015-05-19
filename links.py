01afa78b701876f9deba55f52feae5dcc2b97909
jaryur <8e:03:9b:14:46:cb:1a:b8:62:60:ac:2d:d0:0d:f9:82>
2015-05-19 18:41:06 +0800

    新增链接分享文件

---
+++ links.py
@@ -0,0 +1,12 @@
+链接分享
+
+【电子书】
+python学习手册=http://pan.baidu.com/s/1nt87zYT
+
+【文章】
+python教程（廖雪峰）=http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000
+
+【博客】
+
+
+【视频】
